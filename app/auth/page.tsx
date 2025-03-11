d73a770c47ca52b36fcccba73c71d11138eca2d9
Vivek <vivekousik8555@gmail.com>
2025-03-11 14:14:37 +0530

    finished the uI for authentication barring the social authentications

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,33 +1,85 @@
+"use client";
 /* eslint-disable @next/next/no-img-element */
-import Input from '@/components/input'
-import React from 'react'
+import Input from "@/components/input";
+import React, { useCallback, useState } from "react";
 
 const AuthPage = () => {
-    return (
-        <div className='relative h-full w-full bg-[url("/images/hero.jpeg")] bg-fixed bg-no-repeat bg-cover bg-center'>
-            <div className='w-full h-full'>
-                <nav className='px-12 py-5'>
-                    <div className='w-full flex gap-2 bg-[#223143]'>
-                        <img src="/images/logo.png" alt="logo" className='h-12' />
-                        <div className='flex gap-0.5 items-center text-2xl'>
-                            <span className='text-white'>Movie</span>
-                            <span className='text-emerald-400 font-medium'>Stream</span>
-                        </div>
-                    </div>
-                    <div className='flex justify-center'>
-                        <div className='bg-black px-16 py-16 self-center mt-10 lg:w-2/5 lg:max-w-md rounded-md w-full'>
-                            <h2 className='text-white text-4xl mb-8 font-semibold text-center'>
-                                Sign In
-                            </h2>
-                            <div className='flex flex-col gap-4'>
-                                <Input />
-                            </div>
-                        </div>
-                    </div>
-                </nav>
-            </div>
-        </div>
-    )
-}
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
 
-export default AuthPage
+  const [variant, setVariant] = useState("login");
+
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) =>
+      currentVariant === "login" ? "signup" : "login"
+    );
+  }, []);
+
+  return (
+    <div className='relative h-full w-full bg-[url("/images/hero.jpeg")] bg-fixed bg-no-repeat bg-cover bg-center'>
+      <div className="w-full h-full">
+        <nav className="px-12 py-5">
+          <div className="w-full flex gap-2 bg-[#223143]">
+            <img src="/images/logo.png" alt="logo" className="h-12" />
+            <div className="flex gap-0.5 items-center text-2xl">
+              <span className="text-white">Movie</span>
+              <span className="text-emerald-400 font-medium">Stream</span>
+            </div>
+          </div>
+          <div className="flex justify-center">
+            <div className="bg-gray-700 px-16 py-16 self-center mt-10 lg:w-2/5 lg:max-w-md rounded-md w-full">
+              <h2 className="text-white text-4xl mb-8 font-semibold text-center">
+                {variant === "login" ? "Sign In" : "Sign Up"}
+              </h2>
+              <div className="flex flex-col gap-4">
+                {variant === "signup" && (
+                  <Input
+                    label="Username"
+                    id="name"
+                    value={name}
+                    onChange={(e: {
+                      target: { value: React.SetStateAction<string> };
+                    }) => setName(e.target.value)}
+                  />
+                )}
+                <Input
+                  label="Email"
+                  id="email"
+                  type="email"
+                  value={email}
+                  onChange={(e: {
+                    target: { value: React.SetStateAction<string> };
+                  }) => setEmail(e.target.value)}
+                />
+                <Input
+                  label="Password"
+                  id="password"
+                  type="password"
+                  value={password}
+                  onChange={(e: {
+                    target: { value: React.SetStateAction<string> };
+                  }) => setPassword(e.target.value)}
+                />
+              </div>
+              <button className="bg-green-500 py-3 text-white rounded-lg cursor-pointer w-full mt-10 hover:bg-green-700 transition">
+                {variant === "login" ? "LogIn" : "Sign Up"}
+              </button>
+              <p className="text-neutral-300 mt-12 text-center">
+                {variant === "login" ? "First time using our MovieStream? " : "Already have an acoount? "}
+                <span
+                  onClick={toggleVariant}
+                  className="text-blue-400 mt-1 font-semibold hover:underline cursor-pointer"
+                >
+                  {variant === "login" ? "Sign Up" : "Log In"}
+                </span>
+              </p>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default AuthPage;
--- components/input.tsx
+++ components/input.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
-const Input = () => {
+interface InputProps {
+    id: string;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    onChange: any;
+    value: string;
+    label: string;
+    type?: string;
+}
+
+const Input = ({ id, onChange, value, label, type }: InputProps) => {
     return (
-        <div>
-            <input type='text' className='bg-white block rounded-md w-full p-1 px-2 text-lg text-gray-800 appearance-none focus:outline-none focus:ring-0' />
+        <div className='relative'>
+            <input value={value} type={type} onChange={onChange} className='bg-white block rounded-md w-full p-1 px-2 text-lg text-gray-800 appearance-none focus:outline-none focus:ring-0 peer' placeholder=' ' id={id}/>
+            <label className='absolute text-lg text-zinc-500 duration-150 transform -translate-y-3 scale-75 top-1 z-10 origin-[0] left-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-50 peer-focus:-translate-y-3' htmlFor={id}>
+                {label}
+            </label>
         </div>
     )
 }
