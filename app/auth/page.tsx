2b017239b5c92d383a2d910edd8d259f6bf6d4b2
Vivek <vivekousik8555@gmail.com>
2025-03-16 17:52:29 +0530

    final tweak, minuscule error fix

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 /* eslint-disable @next/next/no-img-element */
-import Input from "@/components/Input";
+import Input from "@/components/Inputs/Input";
 import axios from "axios";
 import React, { useCallback, useState } from "react";
 import toast from "react-hot-toast";
